1dc0f725729aed18c088533af9a576f90f01879b
Ivan Czar <ivan.czar@uirev.com>
2023-02-02 23:22:58 +1300

    wip: created responsive ui

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+
+const LandingPage = () => {
+  return (
+    <div style={{"height": "900px", "width": "100%"}}>
+        <Navbar/>
+        LandingPage
+        
+        
+        </div>
+  )
+}
+
+
+
+
+export default LandingPage
