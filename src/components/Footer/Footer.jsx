d7998a9b6c06a24662f06e1a98983c34cd936740
Peter An <75343007+Exotica0122@users.noreply.github.com>
2023-02-04 11:50:18 +1300

    responsive footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import './Footer.css';
-import Logo from '../../assets/images/Transparent_CSEA.png';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from "react";
+import Logo from "../../assets/images/Transparent_CSEA.png";
+
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import "./Footer.css";
+
 const Footer = () => {
   return (
-    <div className='footerContainer'>
-      <div className='logoSocialsContainer'>
-        <div>
-          <img src={Logo} alt='Logo' className='logo' />
-        </div>
+    <div className="footer-container">
+      <div className="footer-logo__socials">
+        <img src={Logo} alt="Logo" className="footer-logo" />
 
-        <div className='socialsWrapper'>
-          <a href='https://www.instagram.com/autcsea/'>
-            <InstagramIcon className='socialIcon' />
+        <div className="footer-social__icons">
+          <a href="https://www.instagram.com/autcsea/">
+            <InstagramIcon sx={{ color: "black" }} fontSize="medium" />
           </a>
-          <a href='https://www.facebook.com/AUTCSEA'>
-            <FacebookIcon className='socialIcon' />
+          <a href="https://www.facebook.com/AUTCSEA">
+            <FacebookIcon sx={{ color: "black" }} fontSize="medium" />
           </a>
-          <a href='https://www.linkedin.com/company/autcsea/mycompany/'>
-            <LinkedInIcon className='socialIcon' />
+          <a href="https://www.linkedin.com/company/autcsea/mycompany/">
+            <LinkedInIcon sx={{ color: "black" }} fontSize="medium" />
           </a>
         </div>
       </div>
 
-      <div className='linksWrapper'>
-        <ul>
-          <li>Go to top</li>
-          <li>About us</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+      <ul className="footer-links__list">
+        <li className="footer-link">Go to top</li>
+        <li className="footer-link">About us</li>
+        <li className="footer-link">Contact</li>
+      </ul>
     </div>
   );
 };
